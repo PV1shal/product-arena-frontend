bea66a3881ae7c53e002377f5e81f53ec13e24bf
Vishal Pugazhendhi <vishal.pugazhendhi@gmail.com>
2024-12-09 00:38:40 -0800

    Standard topnav padding

---
+++ src/app/comparison/page.js
@@ -0,0 +1,7 @@
+export default function Comparison() {
+    return (
+        <div className="min-h-screen w-screen flex flex-col items-center">
+            Hello! Welcome to comparison page!
+        </div>
+    );
+}
