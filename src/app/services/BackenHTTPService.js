aeb2d844f59f408d9853e4b94a7f5326b8d112ae
Vishal Pugazhendhi <vishal.pugazhendhi@gmail.com>
2024-12-15 20:58:50 -0800

    can access .env

--- src/app/services/BackenHTTPService.js
+++ src/app/services/BackenHTTPService.js
@@ -3,14 +3,14 @@ import axios from "axios";
 function BackendHttpServices() {
     function getProductComparison({ itemLink1, itemLink2 }) {
         console.log(itemLink1, itemLink2)
-        return axios.post("http://localhost:8000/compare", {
+        return axios.post(process.env.NEXT_PUBLIC_BE_URI_COMPARE, {
             links: [itemLink1, itemLink2]
         });
     }
 
     function getProductComparisonNewApp({ prev_products, new_product }) {
         console.log(prev_products, new_product)
-        return axios.post("http://localhost:8000/compareWithNew", {
+        return axios.post(process.env.NEXT_PUBLIC_BE_URI_NEW_COMPARE, {
             prev_products: prev_products,
             new_product: new_product
         });
