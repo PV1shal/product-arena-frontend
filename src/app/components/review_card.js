654e5e62bef053a2d3b00acfffbf99299954e80b
Vishal Pugazhendhi <vishal.pugazhendhi@gmail.com>
2024-12-16 20:42:04 -0800

    Quotes

--- src/app/components/review_card.js
+++ src/app/components/review_card.js
@@ -4,7 +4,7 @@ const ReviewCard = ({ review }) => {
       <div className="flex items-center mb-2">
         <span className="font-semibold text-blue-600">{review.platform}</span>
       </div>
-      <p className="text-sm italic">"{review.review}"</p>
+      <p className="text-sm italic">&quot;{review.review}&quot;</p>
     </div>
   );
 };
